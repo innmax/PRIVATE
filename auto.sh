37d7dc42e763d168ac36518165093b30e3b31aa8
innmax <innmaxinnmax@gmail.com>
2016-06-10 12:00:25 +0800

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,10 +1,10 @@
 # install pritunl
-wget -O /etc/apt/sources.list.d/mongodb-org-3.0.list "https://raw.github.com/innmax/PRIVATE/master/mongodb"
-wget -O /etc/apt/sources.list.d/pritunl.list "https://raw.github.com/innmax/PRIVATE/master/pritunl"
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7F0CEB10
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv CF8E292A
-apt-get update
-apt-get -y install pritunl mongodb-org
+wget -O /etc/apt/sources.list.d/mongodb-org-3.0.list "https://raw.github.com/innmax/PRIVATE/master/mongodb";
+wget -O /etc/apt/sources.list.d/pritunl.list "https://raw.github.com/innmax/PRIVATE/master/pritunl";
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7F0CEB10;
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv CF8E292A;
+apt-get update;
+apt-get -y install pritunl mongodb-org;
 service pritunl start
 
 # install squid3
