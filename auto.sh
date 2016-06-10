53ce8105f988c2c458b36adc37b8add792e998e2
innmax <innmaxinnmax@gmail.com>
2016-06-10 22:52:14 +0800

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -16,12 +16,30 @@ wget -O /etc/apt/sources.list "https://raw.githubusercontent.com/innmax/PRIVATE/
 wget -q http://www.webmin.com/jcameron-key.asc -O- | apt-key add -;
 apt-get update;
 apt-get -y install webmin;
+# setting vnstat
+vnstat -u -i venet0
+service vnstat restart
+# install vnstat gui
+cd /home/vps/public_html/
+wget http://www.sqweek.com/sqweek/files/vnstat_php_frontend-1.5.1.tar.gz
+tar xf vnstat_php_frontend-1.5.1.tar.gz
+rm vnstat_php_frontend-1.5.1.tar.gz
+mv vnstat_php_frontend-1.5.1 vnstat
+cd vnstat
+sed -i 's/eth0/venet0/g' config.php
+sed -i "s/\$iface_list = array('venet0', 'sixxs');/\$iface_list = array('venet0');/g" config.php
+sed -i "s/\$language = 'nl';/\$language = 'en';/g" config.php
+sed -i 's/Internal/Internet/g' config.php
+sed -i '/SixXS IPv6/d' config.php
+cd
 echo "install pritunl complete"
 service pritunl restart;
 echo "install mogodb complete"
 echo "install squid3 complete"
 service squid3 restart;
 echo "install webmin complete"
+echo "install vnstat complete"
+service vnstat restart
 echo "login to pritunl https://ip:443"
 echo "login to webmin https://ip:10000"
 echo "pritunl key kat bawah ni copy dan paste"
