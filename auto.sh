09e2cf93d417cc8ec4dec0859eb886867ff7e1e8
innmax <innmaxinnmax@gmail.com>
2016-06-10 11:50:36 +0800

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,7 +1,6 @@
 # install pritunl
 wget -O /etc/apt/sources.list.d/mongodb-org-3.0.list "https://raw.github.com/innmax/PRIVATE/master/mongodb"
-
-wget -O nano /etc/apt/sources.list.d/pritunl.list "https://raw.github.com/innmax/PRIVATE/master/pritunl"
+wget -O /etc/apt/sources.list.d/pritunl.list "https://raw.github.com/innmax/PRIVATE/master/pritunl"
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7F0CEB10
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv CF8E292A
 apt-get update
